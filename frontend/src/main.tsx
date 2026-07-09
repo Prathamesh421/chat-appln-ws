f7b9a49aef42a5c4c1d6c2f0a4424bcc62c5039a
Prathamesh421 <prathugavandhare@gmail.com>
2026-07-09 20:15:07 +0530

    toaster config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
-    
+        <>
+        <Toaster position='top-left' toastOptions={{
+                style: {
+                        width: "210px",
+                        height:"45px",
+                        fontSize: "14px"
+                }
+        }}/>
         <App />
+        </>
     
 )
